c90809681467a0b10c7c2820e783f14d2f8ebe95
Tim Thompson <timt333@gmail.com>
2022-02-16 09:21:56 +0900

    added admin/dashboard and logo

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -2,10 +2,14 @@ import { EnterNameComponent } from './pages/enter-name/enter-name.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EnterClassroomIdComponent } from './pages/enter-classroom-id/enter-classroom-id.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
 const routes: Routes = [
   {path: '', component: EnterClassroomIdComponent},
-  {path: 'classroom/:classroomId', component: EnterNameComponent}
+  {path: 'classroom/:classroomId', component: EnterNameComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: 'createaccount', component: CreateAccountComponent}
 ];
 
 @NgModule({
